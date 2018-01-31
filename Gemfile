248a87d053b9ef814c727261c12e47d4afbe5efd
Villager <elvis4600@gmail.com>
2018-01-31 13:31:06 +0800

    update gem version and add settings

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'awesome_nested_set'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# settings
+gem 'settingslogic'
+
 group :development, :test do
 # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,96 +12,96 @@
 
 ActiveRecord::Schema.define(version: 20160716124409) do
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "account",         limit: 255
-    t.boolean  "del_mark",                    default: false
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.string   "password_digest", limit: 255
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "account"
+    t.boolean  "del_mark",        default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "password_digest"
   end
 
-  create_table "attachments", force: :cascade do |t|
-    t.string   "attachmenttable_type", limit: 255
-    t.string   "attachmenttable_id",   limit: 255
-    t.string   "file",                 limit: 255
-    t.boolean  "del_mark",                         default: false
-    t.integer  "rank",                 limit: 4
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
+  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "attachmenttable_type"
+    t.string   "attachmenttable_id"
+    t.string   "file"
+    t.boolean  "del_mark",             default: false
+    t.integer  "rank"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.integer  "category_type", limit: 4
-    t.integer  "rank",          limit: 4
-    t.boolean  "del_mark",                  default: false
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "category_type"
+    t.integer  "rank"
+    t.boolean  "del_mark",      default: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
-  create_table "contents", force: :cascade do |t|
-    t.string   "title",        limit: 255
-    t.integer  "category_id",  limit: 4
-    t.integer  "content_type", limit: 4
+  create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.integer  "category_id"
+    t.integer  "content_type"
     t.text     "content",      limit: 65535
-    t.string   "intro",        limit: 255
-    t.integer  "rank",         limit: 4,     default: 0
+    t.string   "intro"
+    t.integer  "rank",                       default: 0
     t.boolean  "del_mark",                   default: false
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.string   "link",         limit: 255
+    t.string   "link",                                                    collation: "latin1_swedish_ci"
   end
 
-  create_table "navs", force: :cascade do |t|
-    t.string   "url",            limit: 255
-    t.integer  "nav_type",       limit: 4
-    t.integer  "rank",           limit: 4
-    t.integer  "parent_id",      limit: 4
-    t.integer  "lft",            limit: 4
-    t.integer  "rgt",            limit: 4
-    t.integer  "depth",          limit: 4,   default: 0
-    t.integer  "children_count", limit: 4,   default: 0
-    t.boolean  "del_mark",                   default: false
-    t.string   "name",           limit: 255
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+  create_table "navs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "url",                                         collation: "utf8_general_ci"
+    t.integer  "nav_type"
+    t.integer  "rank"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth",          default: 0
+    t.integer  "children_count", default: 0
+    t.boolean  "del_mark",       default: false
+    t.string   "name",                                        collation: "utf8_general_ci"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "fb_name",     limit: 255
-    t.string   "study_at",    limit: 255
-    t.string   "phone",       limit: 255
-    t.string   "price",       limit: 255
-    t.string   "product_id",  limit: 255, default: "--- []\n"
-    t.string   "count",       limit: 255, default: "--- []\n"
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
-    t.string   "trace_fb_id", limit: 255
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "fb_name"
+    t.string   "study_at"
+    t.string   "phone"
+    t.string   "price"
+    t.string   "product_id",  default: "--- []\n"
+    t.string   "count",       default: "--- []\n"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "trace_fb_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "price",       limit: 255
-    t.string   "intro",       limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "category_id", limit: 4
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "price"
+    t.string   "intro"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
-  create_table "systems", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "title",       limit: 255
-    t.string   "description", limit: 255
-    t.string   "keyword",     limit: 255
-    t.string   "email",       limit: 255
-    t.string   "phone",       limit: 255
-    t.string   "fax",         limit: 255
-    t.string   "address",     limit: 255
-    t.string   "facebook",    limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+  create_table "systems", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "description"
+    t.string   "keyword"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "address"
+    t.string   "facebook"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
